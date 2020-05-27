09ebbd5d8e063f6560e047cf25a5995e262de113
FIRST_NAME LAST_NAME <linlin.marcos@gmail.com>
2020-05-28 00:18:10 +0200

    Events created

--- server.js
+++ server.js
@@ -2,7 +2,8 @@ const express = require('express')
 const path = require('path')
 const http = require('http')
 const socketio = require('socket.io')
-
+const calendarDaysMonth = require('./utils/functions.js')
+const Event = require('./utils/models.js')
 
 // Initialization
 const app = express()
@@ -38,89 +39,29 @@ io.on('connection', socket => {
         socket.emit('month_calendar', output)
         console.log(`Month calendar sent`);
     })
+    // Create a event
+    // const event = new Event()
+    // event.title = 'asda'
+    // event.description = 'tora'
+    // event.status = true
+    // event.save()
 
-    // Calculate month days (monday = 1, sunday = 0, satuday = -1)
-    function calendarDaysMonth(clientDate, weekStartingDay) {
-        const currentYear = clientDate.getFullYear()
-        const currentMonth = clientDate.getMonth()
-        const currentDay = clientDate.getDate();
+    // update a event
+    // let { id } = ; // insert ID
+    // event.title = 'asda'
+    // event.description = 'tora'
+    // event.status = true
+    // Event.update({_id: id});
 
-        const months = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December']
-        const monthDaysNumber = [31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31]
-        let a;
-        if (weekStartingDay === 'Monday') {
-            a = 1
-        }
-        if (weekStartingDay === 'Sunday') {
-            a = 0
-        }
-        if (weekStartingDay === 'Saturday') {
-            a = -1
-        }
-
-        // check leap year 
-        if (leapYear(currentYear)) {
-            monthDaysNumber[1] = 29
-        }
-
-        // previous, current and next month names
-        const previousMonth = months[clientDate.getMonth() - 1]
-        const currentMonthName = months[currentMonth]
-        const nextMonth = months[clientDate.getMonth() + 1]
-
-        // first day of the actual month, it's weekday and the date in miliseconds
-        const firstDayOfTheMonth = new Date(currentYear, currentMonth, 1)
-        const miliseconds = firstDayOfTheMonth.getTime()
-        //console.log(firstdayofthemonth)
-        const firstDayWeekDay = firstDayOfTheMonth.getDay()
-        //console.log(firstdayweekday)
-
-        // Calculation of the days to send to frontend
-        const days = []
-        // past month days
-        for (let i = firstDayWeekDay - a; i > 0; i--) {
-            //console.log(i)
-            const pastDays = new Date(miliseconds - 86400000 * i).getDate()
-            days.push(pastDays)
-        }
-        //current month days
-        for (let i = 1; i < monthDaysNumber[currentMonth] + 1; i++) {
-            days.push(i)
-        }
-        //next month days
-        for (let i = 1; i < 15; i++) {
-            if (days.length < 42) {
-                days.push(i)
-            }
-        }
+    // delete event
+    // let { id } =  // gather ID
+    // await Task.remove({_id: id}); //
+    
+    // find events
+    //const tasks = await Task.find();
 
 
-        // delete the days to adjust to 35 or 28
-        const endOfMonth = days.indexOf(monthDaysNumber[currentMonth])
-        if (endOfMonth < 35) {
-            days.splice(35, 41)
-        }
-
-        if (endOfMonth < 28) {
-            days.splice(28, 41)
-        }
-
-        const output = {}
-        output.currentYear = currentYear
-        output.currentMonth = currentMonthName
-        output.currentDay = currentDay
-        output.previousMonth = previousMonth
-        output.nextMonth = nextMonth
-        output.weekStartingDay = weekStartingDay
-        output.days = days
-        // console.log(days)
-        return output;
-    }
-
-    // Check leap year
-    function leapYear(year) {
-        return ((year % 4 == 0) && (year % 100 != 0)) || (year % 400 == 0);
-    }
+    
 
     // Calculate week days
     function calendarDaysWeek(clientDate, weekStartDay) {
