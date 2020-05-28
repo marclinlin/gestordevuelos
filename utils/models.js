54a32218d239fa0f9a346e54934d73d26fe4137d
FIRST_NAME LAST_NAME <linlin.marcos@gmail.com>
2020-05-28 12:47:45 +0200

    Added models

--- utils/models.js
+++ utils/models.js
@@ -2,8 +2,13 @@ const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
 const EventSchema = Schema({
+  typeOfEvent: String,
   title: String,
+  startTime: Object,
+  endTime: Object,
   description: String,
+  instructor: String,
+  student: String,
   status: {
     type: Boolean,
     default: false
