e404f6c3a2ffd7c2788ef9fff2078c0d316f30fc
FIRST_NAME LAST_NAME <linlin.marcos@gmail.com>
2020-06-03 23:00:59 +0200

    db cloud

--- database.js
+++ database.js
@@ -1,5 +1,7 @@
 const mongoose = require('mongoose');
 
+
+// To connect to cloud database: mongodb+srv://user:23091994Se_@cluster0-5u7zi.mongodb.net/test?retryWrites=true&w=majority
 mongoose.connect('mongodb://localhost/db', {
    useCreateIndex: true,
    useNewUrlParser: true,
